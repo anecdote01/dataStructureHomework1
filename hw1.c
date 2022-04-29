d5a9fe89de61e0e60c402bf3cf24f801d06bcaa3
mehmet soner korucu <mehmetsonerkorucu@hotmail.com>
2022-04-29 10:43:00 +0300

    Revert "bir şeyler denedim"
    This reverts commit 0a35404e5d103110be159f164c2af14387dbe402.

--- hw1.c
+++ hw1.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -16,8 +8,9 @@ void ft_deletion();
 
 struct l_node{
     int num;
+    char *name;
     struct l_node* next;
-};
+}*head = NULL;
 
 struct s_node {
     struct l_node* data;
@@ -26,7 +19,10 @@ struct s_node {
 
 int*	ft_sort_int_tab(int *tab, int size)
 {
-	int	y = 0, swap;
+	int	y;
+	int	swap;
+
+	y = 0;
 	while (y < size - 1)
 	{
 		if (tab[y] > tab[y + 1])
@@ -44,11 +40,11 @@ int*	ft_sort_int_tab(int *tab, int size)
 //pushing new student linked list into stack
 void ft_push(struct l_node *studentLink)
 {
-    printf("inside push\n");
     struct s_node *newStudent;
     newStudent->data = studentLink;
     newStudent->link = stack;
     stack = newStudent;
+    printf("inside push\n");
 }
 
 void ft_deletion()
@@ -61,59 +57,31 @@ void ft_deletion()
 void ft_sort_year()
 {
     int year_sort_array[50], i= 0, *year_sorted_array, y = 0;
-    struct s_node *current = stack;
-    while (current == NULL)
+    struct s_node *temp = stack;
+    while (temp == NULL)
     {
-        struct l_node *temp = current->data;
-        year_sort_array[i++] = ((temp->num) % 1000000);
-        current = current->link;
+        year_sort_array[i++] = ((temp->data->num) % 1000000);
+        temp = temp->link;
     }
     year_sorted_array = ft_sort_int_tab(year_sort_array, i);
     while (y < i)
     {
         struct s_node *sorting = stack;
-        if (year_sorted_array[y++] == (sorting->data->num) % 1000000)
+        if ( year_sorted_array[y] == (sorting->data->num) % 1000000)
         {
-            printf("%d\t",sorting->data->num);
-            sorting->data = sorting->data->next;
-            while (sorting->data->next)
-            {
-                printf("%c",(char)(sorting->data->num));
-                sorting->data = sorting->data->next;
-            }
+            printf("%d\t%s\n",sorting->data->num,sorting->data->name);
         }
-        sorting = sorting->link;
     }
+    
 }
-int	ft_strlen(char *str)
+
+struct l_node *ft_input(char* st_name, int st_id)
 {
-	int	i = 0;
-	while (str[i] != '\0')
-	{
-	    i++;
-	}
-	printf("inside strlen\n");
-	return (i);
-}
-struct l_node *ft_input(char *st_name, int st_id)
-{
-    printf("%s",st_name);
-    struct l_node *head = NULL, *id_node;
-    int len = ft_strlen(st_name);
     printf("inside input\n");
-    while (len > 0)
-    {
-        printf("inside loop\n");
-        struct l_node *newnode;
-        newnode->num = (int)(st_name[--len]);
-        newnode->next = head;
-        head = newnode;
-    }
-    id_node->num = st_id;
-    id_node->next = head;
-    head = id_node;
-    printf("outside input\n");
-    ft_push(head);
+    struct l_node *new_student;
+    new_student->num = st_id;
+    new_student->name = st_name;
+    return (new_student);
 }
 void theMenu(void)
 {
@@ -134,7 +102,8 @@ void theMenu(void)
                 scanf("%d",&idnum);
                 printf("\nenter name: ");
                 scanf("%s", name);
-                ft_input(name,idnum);
+                ft_push(ft_input(name,idnum));
+                printf("outside push\n");
         case 2: ft_sort_id;
         case 3: ft_sort_year;
         case 4: ft_sort_faculty;
