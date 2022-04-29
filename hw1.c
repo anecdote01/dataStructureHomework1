5244c51ac58dbe4b037ae16db542b31157658d04
mehmet soner korucu <mehmetsonerkorucu@hotmail.com>
2022-04-29 15:51:44 +0300

    yaptım ama bozuk, fatihinkine benziyor

--- hw1.c
+++ hw1.c
@@ -1,124 +1,159 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void ft_sort_id();
-void ft_sort_year();
-void ft_sort_faculty();
-void ft_deletion();
-
-struct l_node{
+struct node{
     int num;
-    char *name;
-    struct l_node* next;
-}*head = NULL;
+    char name[50];
+    struct node* next;
+};
 
-struct s_node {
-    struct l_node* data;
-    struct s_node* link;
-} *stack=NULL;
-
-int*	ft_sort_int_tab(int *tab, int size)
+struct node* ft_deletion(struct node *list,int delnum)
 {
-	int	y;
-	int	swap;
-
-	y = 0;
-	while (y < size - 1)
-	{
-		if (tab[y] > tab[y + 1])
-		{
-			swap = tab[y + 1];
-			tab[y + 1] = tab[y];
-			tab[y] = swap;
-			y = 0;
-		}
-		else
-			y++;
-	}
-    return (tab);
-}
-//pushing new student linked list into stack
-void ft_push(struct l_node *studentLink)
-{
-    struct s_node *newStudent;
-    newStudent->data = studentLink;
-    newStudent->link = stack;
-    stack = newStudent;
-    printf("inside push\n");
-}
-
-void ft_deletion()
-{
+    struct node *curr = list, *freenode;
     printf("inside deletion\n");
-    if(stack == NULL) printf("no data found\n");
-    else stack = stack->link;
+    if(list == NULL) 
+    {
+        printf("no data found to delete!\n");
+        return list;
+    }
+    if(curr->num == delnum)
+    {
+        freenode = curr;
+        curr = curr->next;
+        free(freenode);
+        return curr;
+    }
+    while (curr->next != NULL)
+    {
+        if(curr->next->num == delnum)
+            break;
+        else
+            curr = curr->next;
+    }
+    if(curr->next == NULL)
+        printf("numbers not matched..\n");
+    else
+    {
+        curr = freenode->next;
+        freenode->next = curr->next->next;
+        free (freenode);
+    }
+    return list;
 }
 
-void ft_sort_year()
+struct node* ft_push(char st_name[50], int st_id,struct node *stack)
 {
-    int year_sort_array[50], i= 0, *year_sorted_array, y = 0;
-    struct s_node *temp = stack;
-    while (temp == NULL)
-    {
-        year_sort_array[i++] = ((temp->data->num) % 1000000);
-        temp = temp->link;
-    }
-    year_sorted_array = ft_sort_int_tab(year_sort_array, i);
-    while (y < i)
-    {
-        struct s_node *sorting = stack;
-        if ( year_sorted_array[y] == (sorting->data->num) % 1000000)
-        {
-            printf("%d\t%s\n",sorting->data->num,sorting->data->name);
-        }
-    }
-    
-}
-
-struct l_node *ft_input(char* st_name, int st_id)
-{
-    printf("inside input\n");
-    struct l_node *new_student;
+    struct node *new_student;
     new_student->num = st_id;
-    new_student->name = st_name;
-    return (new_student);
-}
-void theMenu(void)
+    strcpy(new_student->name , st_name);
+    new_student->next = stack;
+    stack = new_student;
+    return stack;
+} 
+
+struct node* ft_sorter(struct node* stack, int mod)
 {
-    //char below is for no use except preventing bugs.
-    char *name;
-    int idnum;
-    int menuInput = 0;
-    printf("1- Enter school number:\n2- Display school numbers sorted by id:\n3- Display school numbers sorted by year\n4- Display school numbers sorted by the faculty codes\n5- Detele a record by school number\n6- Exit\n");
-    printf("enter choice: ");
-    if(scanf("%d",&menuInput)<= 0)
+    struct node *sorted_stack = malloc(sizeof(struct node));
+    struct node *curr, *min_num;
+    int id;
+    sorted_stack = NULL;
+    if(stack)
+        return stack;
+    while (stack)
     {
-        printf("please write a valid choice:");
-        exit(0);
+        curr = stack;
+        id = curr->num;
+        min_num = curr;
+        while (curr)
+        {
+            if((id % mod) <= (curr->num % mod))
+                curr = curr->next;
+            else
+            {
+                min_num = curr;
+                id = min_num->num;
+                curr = curr->next;
+            }
+        }
+        sorted_stack = ft_push(min_num->name,min_num->num,sorted_stack);
+        stack = ft_deletion(stack,min_num->num);
     }
-    switch(menuInput)
-    {
-        case 1: printf("\nenter school number: ");
-                scanf("%d",&idnum);
-                printf("\nenter name: ");
-                scanf("%s", name);
-                ft_push(ft_input(name,idnum));
-                printf("outside push\n");
-        case 2: ft_sort_id;
-        case 3: ft_sort_year;
-        case 4: ft_sort_faculty;
-        case 5: ft_deletion;
-        case 6: exit(0);
-    }
+    return sorted_stack;
 }
-    
 
 int main()
 {
+    struct node *stack = NULL;
+    struct node *curr;
+    char name[50];
+    char surname[50];
+    int idnum;
+    int menuInput;
+    printf("1- Enter school number:\n2- Display school numbers sorted by id:\n3- Display school numbers sorted by year\n4- Display school numbers sorted by the faculty codes\n5- Detele a record by school number\n6- Exit\n");
     while(1)
     {
-    theMenu();
-    system("cls");
+        printf("enter choice: ");
+        scanf("%d",&menuInput);
+        if(menuInput <= 0 & menuInput >=7)
+        {
+            printf("please write a valid choice between 1-5\n");
+            break;
+        }
+        else
+        {
+            switch(menuInput)
+            {
+                case 1: printf("\nenter school number: ");
+                        scanf("%d",&idnum);
+                        if((idnum / 100000000) <= 0 || (idnum / 100000000) > 9)
+                            {
+                                printf("invalid id!\n");
+                                break;
+                            }
+                        printf("enter first name: ");
+                        scanf("%s", name);
+                        printf("enter surname: ");
+                        scanf("%s",surname);
+                        strcat(name," ");
+                        strcat(name,surname);
+                        stack = ft_push(name,idnum,stack);
+                        break;
+                case 2: 
+                    stack = ft_sorter(stack,10000);
+                    curr = stack;
+                    printf("////////Sorted by ID:\\\\\\\\\n");
+                    break;
+                case 3: 
+                    stack = ft_sorter(stack,1000000);
+                    curr = stack;
+                    printf("///////Sorted by year\\\\\\\\\\\n");
+                case 4: 
+                    stack = ft_sorter(stack,100000000);
+                    curr = stack;
+                    printf("///////Sorted by faculty code\\\\\\\\\\\n");
+                case 5:
+                    printf("enter school number you want to delete: ");
+                    scanf("%d",&idnum);
+                    stack = ft_deletion(stack,idnum);
+                    break;
+                case 6: 
+                    while(stack)
+                    {
+                        printf("%d\t%s\n",curr->num,curr->name);
+                        curr = curr->next;
+                    }       
+                    //exit(0);
+            }
+            if (menuInput <= 4 && menuInput >= 2)
+            {
+                while (curr)
+                {
+                    printf("%d\t%s\n",curr->num,curr->name);
+                    curr = curr->next;
+                }
+            }
+        }
     }
     return 0;
 }
